c61c7dc44bd9d04ed01059ede3b08a060646dd94
Parsa <deadlockfree504@gmail.com>
2024-12-11 14:14:11 +0330

    models.py

--- listing/models.py
+++ listing/models.py
@@ -1,7 +1,39 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
+    
 
 
+class City(models.Model):
+    city = models.CharField(max_length=110)
+
+    def __str__(self):
+        return f"{self.city}"
+
+
+class Place(models.Model):
+    section = models.CharField(max_length=110)
+    city = models.ForeignKey(City,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f"{self.city}"
+
+class Listing(models.Model):
+    title = models.CharField(max_length=100)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    place = models.ForeignKey(Place,on_delete=models.CASCADE)
+    price = models.IntegerField(default=0)
+    description = models.TextField(default='')
+    contact = models.CharField(max_length=11)
+
+    def __str__(self):
+        return f"{self.title} - {self.category}"
+    
 
 
