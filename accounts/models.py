f4aa6345fa763d613332e94c3c1c2b7c5101d031
Parsa <deadlockfree504@gmail.com>
2024-12-11 15:24:39 +0330

    added customusermanager

---
+++ accounts/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, phone_number, password=None, **extra_fields):
+        if not username:
+            raise ValueError('The Username field must be set')
+        if not phone_number:
+            raise ValueError('The Phone Number field must be set')
+
+        user = self.model(username=username, phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, phone_number, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=50, unique=True)
+    phone_number = models.CharField(max_length=11, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['phone_number']
+
+    def __str__(self):
+        return self.username
