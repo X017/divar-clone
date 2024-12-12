4451b738ee078c7110059dcf05a71cf5558d390d
Parsa <deadlockfree504@gmail.com>
2024-12-12 13:34:22 +0330

    debug-1

--- accounts/models.py
+++ accounts/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-
+print("#[@] Started CustomUser")
 class CustomUserManager(BaseUserManager):
+    print('#[@] BaseUserManager started')
     def create_user(self, username, phone_number, password=None, **extra_fields):
+        print("create user called")
         if not username:
             raise ValueError('The Username field must be set')
         if not phone_number:
             raise ValueError('The Phone Number field must be set')
 
         user = self.model(username=username, phone_number=phone_number, **extra_fields)
-        user.set_password(password)
+        print(user.password)
+        user.set_password(password)  # Hashes the password
+
         user.save(using=self._db)
         return user
 
@@ -22,11 +26,16 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self.create_user(username, phone_number, password, **extra_fields)
+        user = self.create_user(username, phone_number, password, **extra_fields)
+        v= user.set_password(password)
+        user.save(using=self._db)
+        print(user)
+        return user
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=50, unique=True)
     phone_number = models.CharField(max_length=11, unique=True)
+    password = models.CharField(max_length=1000)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
