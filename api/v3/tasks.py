ca06f543ca847b4fa5eb7d7a57f175478cf9b82c
Parsa <deadlockfree504@gmail.com>
2024-12-26 16:35:01 +0330

    updated tasks.py

--- api/v3/tasks.py
+++ api/v3/tasks.py
@@ -1,12 +1,15 @@
 
 from __future__ import absolute_import , unicode_literals
 from celery import shared_task
-from .utils import code2fa
+import requests
+
 
 
 @shared_task()
-def start_otp(note):
-    note = code2fa()
-    print(note)
+def start_otp(note,receiver_number):
+    final = "DRF Activation CODE: " + note
+    url = f"http://sms.segalnet.net/send_via_get/send_sms.php?note={final}&username=bonit_ad&password=&receiver_number={receiver_number}&sender_number=9890008750"
+    requests.get(url)
+    
     return note
     
